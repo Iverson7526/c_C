82630c7f69ee45e7f47045dd534cfd0b05cc342e
iverson <iverson7526@163.com>
2018-08-07 22:22:20 +0800

    index-swiper

---
+++ go/src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for = "item of swiperList" :key = "item.id"><img class="swiper-img" :src = "item.imgUrl"  alt=""></swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        },
+        loop: true,
+        autoplay: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/3a/48a1c53041cd9602.jpg_750x200_e36210c1.jpg'
+      },
+      {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/1d/187ca571d2f9d602.jpg_750x200_d163f927.jpg'
+      },
+      {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1807/c6/44fce1467be17702.jpg_750x200_406f5fc3.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff
+  .wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 26.66%
+    background #ccc
+    .swiper-img
+      width 100%
+</style>
