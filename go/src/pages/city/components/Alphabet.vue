75b83391daf2134496d5afad7e5283b0b4d76f4b
iverson <iverson7526@163.com>
2018-08-15 23:49:03 +0800

    city-alphabet-touchmove

--- go/src/pages/city/components/Alphabet.vue
+++ go/src/pages/city/components/Alphabet.vue
@@ -1,6 +1,14 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) of cities" :key="key" @click="handleClick">{{key}}</li>
+    <li class="item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @click="handleClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+    >{{item}}</li>
   </ul>
 </template>
 
@@ -10,9 +18,39 @@ export default {
   props: {
     cities: Object
   },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
   methods: {
     handleClick (e) {
       this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 75
+        const index = Math.floor((touchY - startY) / 20)
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit('change', this.letters[index])
+        }
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
     }
   }
 }
